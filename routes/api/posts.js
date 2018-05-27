24519959a5f9d77cdb59c0bc92c9f82ddffea744
Arjun <arjun.pydi@ebutor.com>
2018-05-27 18:23:35 +0530

    Added Comment and Delete Comment APIs

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -117,4 +117,26 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
         })
 });
 
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Comment to a post
+// @access  Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            // Check to see if Comment Exists
+            if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
+                return res.status(400).json({ commentnotexists: "Comment does not exist" })
+            }
+            // Find index to delete post
+            const removeIndex = post.comments
+                .map(item => item._id.toString())
+                .indexOf(req.params.comment_id);
+            // Adding that to the post
+            post.comments.splice(removeIndex, 1);
+            // Save the post finally
+            post.save().then(res => res.status(200).json(post));
+        })
+        .catch(post => res.status(404).json({ postnotfound: "No post found" }))
+});
+
 module.exports = router;
