a1f6d5abafd33b3e6c872c452792b820010b73dd
Arjun <arjun.pydi@ebutor.com>
2018-05-27 14:29:45 +0530

    Added Delete REST API for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,11 +4,29 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // Validation Import
 const validationPostInput = require('../../validation/post');
 
-router.get('/test',(req,res) => (res.json(200,{"msg": "Posts api looks cool"})))
+// @route   POST api/posts
+// @desc    View all Posts
+// @access  Public
+router.get('/',(req,res) => {
+    Post.find()
+        .sort({data: -1})
+        .then(posts => res.status(200).json(posts))
+        .catch(err => res.status(404).json({ nopostsfound: "No posts found!" }));
+})
+
+// @route   POST api/posts/:id
+// @desc    View Single Posts
+// @access  Public
+router.get('/:id',(req,res) => {
+    Post.findById(req.params.id)
+        .then(post => res.status(200).json(post))
+        .catch(err => res.status(404).json({ nopostfound: "No post found!" }));
+})
 
 // @route   POST api/posts
 // @desc    Create Post
@@ -29,6 +47,28 @@ router.post('/',passport.authenticate('jwt', { session: false }), (req, res) => 
     });
 
     newPost.save().then(post => res.status(200).json(post));
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete Post
+// @access  Private
+router.delete('/:id',passport.authenticate('jwt',{ session:false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // Check for POST
+                    if(post.user.toString() !== req.user.id) {
+                        return res
+                            .status(401)
+                            .json({ notauthorized: "You are not authorized to delete the post"});
+                    }
+
+                    // Delete
+                    post.remove().then(() => res.json({success: true}));
+                })
+                .catch(post => res.status(404).json({ postnotfound: "No posts found to delete"}))
+        })
 });
 
 module.exports = router;
